cc02e13d9c67d9c76780e641352b39f559110e45
z115390 <123448610+z115390@users.noreply.github.com>
2023-03-21 09:21:53 +0100

    Add files via upload

--- Penjat.js
+++ Penjat.js
@@ -1,33 +1,47 @@
-           var Vides ="7";
-           var Paraula ="";
-           var Lletra ="";   
+    
+    
+    // variables
+    
+    var Vides ="7";
+    var Paraula ="";
+    var Lletra ="";   
+    var paraules = ["fetge", "força", "jutges", "jutjat", "mengen", "penjat", "quinta", "setze"];             
+    var Paraula = paraules[Math.floor(Math.random()* paraules.length)];           
+    var pos = Paraula.indexOf(Lletra);              
+    var endevinades = []; 
+    for (var i = 0; i < Paraula.lengt; i++){
+        i = "_"
+        
+    }
+    
+     function Comprovar(){
+      var Lletra = document.getElementById("Lletra").value; 
 
-        function Comprovar(){
-           var Lletra = document.getElementById("Lletra").value; 
-
-         if ((Lletra >= "a") && (Lletra <= "m")){
-                document.getElementById("resultat").innerHTML = "encert"; 
+      if ((Lletra >= "a") && (Lletra <= "m")){
+        document.getElementById("resultat").innerHTML = "encert"; 
                 alert('Lletra' + " has encert "); }
 
-         else {  
+      else {  
 
-                document.getElementById("resultat").innerHTML = "fallat";
-
-                window.alert('Lletra' + " has fallat ");
-            }
-            }
+        document.getElementById("resultat").innerHTML = "fallat";
+        window.alert('Lletra' + " has fallat ");
+        }
+        
+        }
             
+            
+    
+         
 function AmagaImg() {
-      document.getElementById("ahorcado_6").hidden = true;
-      document.getElementById("ahorcado_5").hidden = true;
-      document.getElementById("ahorcado_4").hidden = true;
-      document.getElementById("ahorcado_3").hidden = true;
-      document.getElementById("ahorcado_2").hidden = true;
-      document.getElementById("ahorcado_1").hidden = true;
       document.getElementById("ahorcado_0").hidden = true;
+      document.getElementById("ahorcado_1").hidden = true;
+      document.getElementById("ahorcado_2").hidden = true;
+      document.getElementById("ahorcado_3").hidden = true;
+      document.getElementById("ahorcado_4").hidden = true;
+      document.getElementById("ahorcado_5").hidden = true;
+      document.getElementById("ahorcado_6").hidden = true;
       
   }
-
 
 function AturaTot(){
     document.getElementById("Lletra").disabled = true; 
@@ -35,3 +49,4 @@ function AturaTot(){
     document.getElementById("Segons").hidden = true; 
 }
  
+         
